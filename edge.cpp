faa686c3e19ff35d228e5fd637deb8913a97f1e0
Gregor Angelow <gregor.angelow@smail.uni-koeln.de>
2020-08-25 19:59:49 +0200

    Removed unnecessary get_... interface.

--- edge.cpp
+++ edge.cpp
@@ -7,6 +7,15 @@
 Edge::Edge(const Vertex &vertex_from, const Vertex &vertex_to) :
     from{ vertex_from }, to{ vertex_to } {}
 
+
+//Vertex Edge::get_from() const {
+//    return from;
+//}
+//Vertex Edge::get_to() const {
+//    return to;
+//}
+
+
 std::ostream& operator<< (std::ostream& out, const Edge &edge) {
 	out << "(" << edge.from << "," << edge.to << ")" ;
 	return out;
--- edge.h
+++ edge.h
@@ -15,6 +15,9 @@ class Edge{
 	//Edge(Vertex &vertex_from, Vertex &vertex_to);
 	Edge(const Vertex &vertex_from, const Vertex &vertex_to);
 
+//	Vertex get_from() const;
+//	Vertex get_to() const;
+
 	
 	friend std::ostream& operator<< (std::ostream& out, const Edge &edge);
 
