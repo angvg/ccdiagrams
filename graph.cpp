0fda07de50452eb20bfd26f2b0b9320c7610b2e2
Gregor Angelow <gregor.angelow@smail.uni-koeln.de>
2020-08-25 21:13:52 +0200

    Reworked constructor from only edges.

--- graph.cpp
+++ graph.cpp
@@ -3,18 +3,32 @@
 
 Graph::Graph( Vertex vertex, std::deque<Edge> edges) :
     m_vertices{ {vertex} }, m_edges{ edges } {};
-Graph::Graph(std::deque<Vertex> vertices, std::deque<Edge> edges) :
+Graph::Graph(std::set<Vertex> vertices, std::deque<Edge> edges) :
     m_vertices{ vertices }, m_edges{ edges } {};
+Graph::Graph( std::deque<Edge> edges ) : m_edges{ edges } {
+    for ( auto& edge : m_edges ) {
+	auto result_from = m_vertices.insert( edge.from );
+	if ( !result_from.second ) {
+	    auto node = m_vertices.extract( result_from.first );
+	    node.value() += edge.from;
+	    m_vertices.insert(std::move(node));
+	    //Vertex add_vertex = *result_from.first;	
+	    //add_vertex += edge.from;
+	    //m_vertices.erase( result_from.first );
+	    //m_vertices.insert( add_vertex );
+	}
+    }
+};
 
 
-void Graph::append_vertex(const Vertex& vertex ) {
-    m_vertices.push_back( vertex );
-}
+//void Graph::append_vertex(const Vertex& vertex ) {
+//    m_vertices.push_back( vertex );
+//}
 
-void Graph::append_vertices(const std::deque<Vertex>& vertices) {
-    for ( auto& vertex : vertices )
-	m_vertices.push_back( vertex );
-}
+//void Graph::append_vertices(const std::deque<Vertex>& vertices) {
+//    for ( auto& vertex : vertices )
+//	m_vertices.push_back( vertex );
+//}
 
 void Graph::append_edge(const Edge& edge ) {
     m_edges.push_back( edge );
@@ -24,7 +38,7 @@ void Graph::append_edges(const std::deque<Edge>& edges ) {
     m_edges.push_back( edge );
 }
 
-std::deque<Vertex> Graph::get_vertices() const {
+std::set<Vertex> Graph::get_vertices() const {
     return m_vertices;
 }
 
@@ -37,12 +51,12 @@ void Graph::print_edges() const {
 	std::cout << edge;
 }
 
-Graph operator+ (const Graph &graph1, const Graph &graph2) {
-    Graph graph( graph1.get_vertices(), graph1.get_edges() );
-    graph.append_vertices( graph2.get_vertices() );
-    graph.append_edges( graph2.get_edges() );
-    return graph;
-}
+//Graph operator+ (const Graph &graph1, const Graph &graph2) {
+//    Graph graph( graph1.get_vertices(), graph1.get_edges() );
+//    graph.append_vertices( graph2.get_vertices() );
+//    graph.append_edges( graph2.get_edges() );
+//    return graph;
+//}
 
 std::ostream& operator<< ( std::ostream &out, const Graph &graph ) {
     out << "Graph ";
--- graph.h
+++ graph.h
@@ -3,26 +3,31 @@
 #include "vertex.h"
 #include "edge.h"
 #include <deque>
+#include <set>
 #include <iostream>
 
 class Graph {
     private:
 	
-	std::deque<Vertex> m_vertices;
+	std::set<Vertex> m_vertices;
 	std::deque<Edge> m_edges;
 
     public:
 
 	Graph( Vertex vertex, std::deque<Edge> edges);
-	Graph(std::deque<Vertex> vertices, std::deque<Edge> edges);
+	Graph( std::set<Vertex> vertices, std::deque<Edge> edges );
+	// Adds vertices of argument edges to m_vertices.
+	// Subsequent vertices appearing after the first are added,
+	// i.e. their edge_tables are added.
+	Graph( std::deque<Edge> edges );
 	
-	void append_vertex(const Vertex& vertex);
-	void append_vertices(const std::deque<Vertex>& vertices);
+//	void append_vertex(const Vertex& vertex);
+//	void append_vertices(const std::deque<Vertex>& vertices);
 
 	void append_edge(const Edge& edge);
 	void append_edges(const std::deque<Edge>& edges );
 
-	std::deque<Vertex> get_vertices() const;
+	std::set<Vertex> get_vertices() const;
 	std::deque<Edge> get_edges() const;
 	
 	void print_edges() const;
@@ -31,4 +36,4 @@ class Graph {
 
 };
 
-Graph operator+ (const Graph &graph1, const Graph &graph2);
+//Graph operator+ (const Graph &graph1, const Graph &graph2);
