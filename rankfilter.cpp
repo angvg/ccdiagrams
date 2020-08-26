1eb8f84e20cc12fb247b46f7819a1906a313de26
Gregor Angelow <gregor.angelow@smail.uni-koeln.de>
2020-08-26 13:42:31 +0200

    rank_filter now returns a queue of sets instead of a queue of queues.

--- rankfilter.cpp
+++ rankfilter.cpp
@@ -2,33 +2,16 @@
 #include <deque>
 #include <iostream>
 
-// Iterative helper function.
-// From a given iterator over the vertex list, check if the current vertex is physical,
-// if yes, append it to the current list and keep track of rank.
-//void iterative_append( std::deque<Vertex>::const_iterator iter, const std::deque<Vertex> &vertexmap,
-//	std::deque<Vertex> &currentlist, int &currentrank, const int &totalrank,
-//	std::deque<std::deque<Vertex>> &graphlist ) {
-//    if ( ( (*iter).get_operatortype() == OperatorType::physical ) &&
-//	    ( (*iter).get_name() != (*(iter-1)).get_name()) ) {
-//	currentlist.push_back( *iter );
-//	currentrank += (*iter).rank();
-//    }
-//	if ( iter != vertexmap.cend() )
-//	    iterative_append( ++iter, vertexmap, currentlist, currentrank, totalrank, graphlist );
-//	else if ( totalrank == currentrank ) {
-//	    graphlist.push_back( currentlist );
-//	}
-//}
 	    
  // The idea here is that for each fragment of a physical operator in a queue,
  // the next physical operator creates a copy of that queue for each of its own
  // fragments and appends the repective fragment to that queue. This results in queue that contains
  // lists (or again queues) of unique fragment combinations.
-std::deque<std::deque<Vertex*>> physical_fragment_combinations(
+std::deque<std::set<Vertex*>> physical_fragment_combinations(
 	std::map<OperatorType,std::deque<Vertex>>& vertexmap ) {
     // We need this dummy in order to have a first entry.
-    std::deque<Vertex*> dummy;
-    std::deque<std::deque<Vertex*>> outqueue{ dummy };
+    std::set<Vertex*> dummy;
+    std::deque<std::set<Vertex*>> outqueue{ dummy };
 
     for ( auto& mapentry : vertexmap ) {
 	if ( mapentry.first == OperatorType::physical ) {
@@ -36,10 +19,10 @@ std::deque<std::deque<Vertex*>> physical_fragment_combinations(
 	    int old_fragmentcount = outqueue.size();
 	    // For each existing fragment in the queue, create new queues from it with a new 
 	    // fragment from another operator appended to it.
-	    for ( auto& fragmentlist : outqueue ) {
+	    for ( auto& fragmentset : outqueue ) {
 		for ( auto& fragment : mapentry.second ) {
-		    std::deque<Vertex*> fragments_up_to_now{ fragmentlist };
-		    fragments_up_to_now.push_back( &fragment );
+		    std::set<Vertex*> fragments_up_to_now{ fragmentset };
+		    fragments_up_to_now.insert( &fragment );
 		    outqueue.push_back( fragments_up_to_now );
 		}
 	    }
@@ -52,13 +35,13 @@ std::deque<std::deque<Vertex*>> physical_fragment_combinations(
 }
 	    
 
-std::deque<std::deque<Vertex>> rank_filter( std::map<OperatorType,std::deque<Vertex>> vertexmap ) {
+std::deque<std::set<Vertex>> rank_filter( std::map<OperatorType,std::deque<Vertex>> vertexmap ) {
     // Remember: Rank here is double the excitation level
     int externalrank{ 0 };
     int clusterrank { 0 };
-    std::deque<std::deque<Vertex>> graphlist;
-    std::deque<Vertex> externallist;
-    std::deque<Vertex> clusterlist;
+    std::deque<std::set<Vertex>> unique_combinations;
+    std::set<Vertex>> externallist;
+    std::set<Vertex>> clusterlist;
 
     // Create all possible combinations of physical operator fragments.
     // Populate external & cluster list and keep track of rank
@@ -68,37 +51,40 @@ std::deque<std::deque<Vertex>> rank_filter( std::map<OperatorType,std::deque<Ver
 	if ( mapentry.first == OperatorType::external ) {
 	    for ( auto& vertex : mapentry.second ) {
 		externalrank += vertex.rank();
-		externallist.push_back( vertex );
+		externallist.insert( vertex );
 	    }
 	}
 	else if ( mapentry.first == OperatorType::cluster ) {
 	    for ( auto& vertex : mapentry.second ) {
 	    clusterrank += vertex.rank();
-	    clusterlist.push_back( vertex );
+	    clusterlist.insert( vertex );
 	    }
 	}
     }
 
     int totalrank{ (-externalrank) - clusterrank };
 
-    std::deque<std::deque<Vertex*>> fragment_combinations( physical_fragment_combinations( vertexmap ) ); 
+    // Create a queue of all possible physical operator fragment combinations
+    std::deque<std::set<Vertex*>> fragment_combinations( physical_fragment_combinations( vertexmap ) ); 
 
-    for ( auto& fragmentlist : fragment_combinations ) {
+    // Now we need to weed out the combinations with a wrong rank.
+    for ( auto& fragmentset : fragment_combinations ) {
 	// Calculate rank
 	int tmprank{0};
-	for ( auto& fragment : fragmentlist ) {
+	for ( auto& fragment : fragmentset ) {
 	    tmprank += fragment->rank();
 	}
 	if ( tmprank == totalrank ) {
-	    std::deque<Vertex> tmpqueue{ externallist };
-	    for ( auto& vertex : fragmentlist )
-		tmpqueue.push_back( *vertex );
+	    // Ranks fits. Now concatenate queues of external, physical and cluster operators.
+	    std::set<Vertex> tmpset{ externallist };
+	    for ( auto& vertex : fragmentset )
+		tmpset.insert( *vertex );
 	    for ( auto& vertex : clusterlist )
-		tmpqueue.push_back( vertex );
-	    graphlist.push_back( tmpqueue );
+		tmpset.insert( vertex );
+	    unique_combinations.push_back( tmpqueue );
 	}
     }
 
-    return graphlist;
+    return unique_combinations;
 
 }
--- rankfilter.h
+++ rankfilter.h
@@ -4,4 +4,15 @@
 #include <deque>
 #include <map>
 
-std::deque<std::deque<Vertex>> rank_filter( std::map<OperatorType,std::deque<Vertex>> vertexmap );
+/// Creates a queue for each unique combination of (physical) operators that can survive according to rank.
+/**
+ * Input
+ * 	A map that contains a queue of vertices, that have the type of the key.
+ *
+ * Output
+ * 	The inner set contains all external vertices, a unique combination
+ * 	of physical operators, that can survive according to rank, and all cluster vertices.
+ * 	The outer queue contains inner sets, that differ in their combination of physical operators.
+ *
+ */
+std::deque<std::set<Vertex>> rank_filter( std::map<OperatorType,std::deque<Vertex>> vertexmap );
