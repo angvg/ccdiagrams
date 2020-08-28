962432404c4be3d2d4c4cc461e8ebe4816bab994
Gregor Angelow <gregor.angelow@smail.uni-koeln.de>
2020-08-28 21:10:59 +0200

    Typo.

--- edge.cpp
+++ edge.cpp
@@ -8,16 +8,16 @@ Edge::Edge(const Vertex &vertex_from, const Vertex &vertex_to) :
     m_from{ vertex_from }, m_to{ vertex_to } {}
 Edge::Edge(const Vertex &vertex_from, const Vertex &vertex_to, LineType type) :
     m_from{ vertex_from }, m_to{ vertex_to }, m_type{type} {
-	m_from.clear_table();
-	m_to.clear_table();
-	if ( m_type == LineType::particle ) {
-	    m_from.set_connection( 0,1, 1 );
-	    m_to.set_connection( 1,0, 1 );
-	}
-	if ( m_type == LineType::hole ) {
-	    m_from.set_connection( 1,1, 1 );
-	    m_to.set_connection( 0,0, 1 );
-	}
+//	m_from.clear_table();
+//	m_to.clear_table();
+//	if ( m_type == LineType::particle ) {
+//	    m_from.set_connection( 0,1, 1 );
+//	    m_to.set_connection( 1,0, 1 );
+//	}
+//	if ( m_type == LineType::hole ) {
+//	    m_from.set_connection( 1,1, 1 );
+//	    m_to.set_connection( 0,0, 1 );
+//	}
     }
 
 
