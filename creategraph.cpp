10a98053e138b6337d6d5aab3bbd0fee6f458a74
Gregor Angelow <gregor.angelow@smail.uni-koeln.de>
2020-09-09 17:26:12 +0200

    Finalized combinations, creategraph and and filter functions.

--- creategraph.cpp
+++ creategraph.cpp
@@ -1,7 +1,12 @@
 #include "creategraph.h"
 #include <deque>
+#include <iomanip>
+#include <utility>
 
-std::deque<Edge> create_edges(const std::deque<Vertex> &vertices) {
+
+
+
+std::deque<Edge> create_edges(const std::set<Vertex> &vertices) {
     std::deque<const Vertex*> creation_space_in;
     std::deque<const Vertex*> creation_space_out;
     std::deque<const Vertex*> annihilation_space_in;
@@ -39,7 +44,7 @@ std::deque<Edge> create_edges(const std::deque<Vertex> &vertices) {
 		v_from.set_connection( 0, 1, 1 );
 		v_to.clear_table();	
 		v_to.set_connection( 1 , 0, 1 );
-		edgelist.push_back( Edge( v_from, v_to ) );
+		edgelist.push_back( Edge( v_from, v_to, LineType::particle ) );
 	    }
 	}
     }
@@ -52,7 +57,7 @@ std::deque<Edge> create_edges(const std::deque<Vertex> &vertices) {
 		v_from.set_connection( 1, 1, 1 );
 		v_to.clear_table();	
 		v_to.set_connection( 0 , 0, 1 );
-		edgelist.push_back( Edge( v_from, v_to ) );
+		edgelist.push_back( Edge( v_from, v_to, LineType::hole ) );
 	    }
 	}
     }
@@ -60,91 +65,316 @@ std::deque<Edge> create_edges(const std::deque<Vertex> &vertices) {
 }
 
 
-std::map<Vertex,std::deque<Edge>> basic_cluster_edges( const std::deque<Vertex> &vertices, const std::deque<Edge> &edges ) {
+std::map<Vertex,std::deque<Edge>> basic_cluster_edges( const std::set<Vertex> &vertices, const std::deque<Edge> &edges ) {
     std::map<Vertex,std::deque<Edge>> cluster_edges;
+    //std::deque<Edge> residual_edges;
     for ( auto& vertex : vertices ) {
 	if ( vertex.get_operatortype() == OperatorType::cluster ) {
 	    // Collect all in- and outgoing edges
 	    std::deque<Edge> current_edges;
 	    for ( auto& edge : edges ) {
-		if ( edge.from == vertex || edge.to == vertex )
+		if ( edge.get_from() == vertex || edge.get_to() == vertex )
 		    current_edges.push_back( edge );
+		//else if ( edge.get_from().get_operatortype() != OperatorType::cluster && edge.get_to().get_operatortype() != OperatorType::cluster ) {
+		//   residual_edges.push_back( edge );
+		//}
 	    }
 	    // Append to return map
 	    cluster_edges.insert( { vertex , current_edges } );
 	}
+	else if ( vertex.get_operatortype() == OperatorType::physical ) {
+	    std::deque<Edge> current_edges;
+	    for ( auto& edge : edges ) {
+		Vertex from{ edge.get_from() };
+		Vertex to{ edge.get_to() };
+		if ( ( from == vertex || to == vertex ) &&
+			( from.get_operatortype() != cluster && to.get_operatortype() != cluster ) )
+		    current_edges.push_back( edge );
+	    }
+	    cluster_edges.insert( { vertex, current_edges } );
+	}
     }
+    // Append residuals
+    //if ( !residual_edges.empty() )
+    //	cluster_edges.insert( { {"R", OperatorType::residual} , residual_edges } ); 
     return cluster_edges;
 }
 
 
-std::map<Vertex,std::deque<Graph>> basic_cluster_fragments( const std::map<Vertex,std::deque<Edge>> &vertexedges ) {
-    std::map<Vertex,std::deque<Graph>> fragment_map;
+std::pair<std::set<Vertex>, bool> subtract_connections(
+	const std::set<Vertex> &lhs_vertices, const std::set<Vertex> &rhs_vertices )
+{
+    std::set<Vertex> subtracted{ lhs_vertices };
+    for ( auto& rhs_vertex : rhs_vertices ) {
+	assert( lhs_vertices.contains( rhs_vertex ) && "In subtract_connections(..): rhs_vertex not found in lhs_vertices." );
+	auto lhs_node = subtracted.extract( rhs_vertex );
+	if ( !lhs_node.value().decrease_connection( rhs_vertex ) )
+	    return {lhs_vertices,false}; 
+	subtracted.insert( std::move(lhs_node) );
+    }
+    return {subtracted,true};
+}
+    
+
+std::map<Vertex,std::deque<std::pair<Graph, std::set<Vertex>>>> basic_cluster_fragments(
+	const std::map<Vertex,std::deque<Edge>> &vertexedges, const std::set<Vertex> &all_vertices )
+{
+    std::map<Vertex,std::deque<std::pair<Graph, std::set<Vertex>>>> fragment_map;
     // Iterate over all vertices
     for ( auto& mapentry : vertexedges ) {
-	// Sort edges in incoming and outgoing
-	std::deque<Edge> incoming;
-	std::deque<Edge> outgoing;
-	for ( auto& edge : mapentry.second ) {
-	    if ( edge.from == mapentry.first )
-		outgoing.push_back( edge );
-	    else if ( edge.to == mapentry.first )
-		incoming.push_back( edge );
-	}
-	// Create all combinations of incoming and outgoing edges
-	std::deque<Graph> graphlist;
-	for ( auto& inedge : incoming ) {
-	    for ( auto& outedge : outgoing ) {
-	graphlist.push_back( { mapentry.first, {inedge,outedge} } );
+	if ( mapentry.first.get_operatortype() == OperatorType::cluster ) {
+	    // Sort edges in incoming and outgoing
+	    std::deque<Edge> incoming;
+	    std::deque<Edge> outgoing;
+	    for ( auto& edge : mapentry.second ) {
+		if ( edge.get_from() == mapentry.first )
+		    outgoing.push_back( edge );
+		else if ( edge.get_to() == mapentry.first )
+		    incoming.push_back( edge );
 	    }
+	    // Create all combinations of incoming and outgoing edges
+	    std::deque<std::pair<Graph, std::set<Vertex>>> graphlist;
+	    for ( auto& inedge : incoming ) {
+		for ( auto& outedge : outgoing ) {
+		    Graph new_fragment{ { inedge, outedge } };
+		    auto subtracted{ subtract_connections( all_vertices, new_fragment.get_vertices() ) };
+		    assert( subtracted.second && "Basic_cluster_fragments(..) : failed to subtract connections." );
+		    graphlist.push_back( { { {inedge,outedge} } , subtracted.first } );
+		}
+	    }
+	    fragment_map.insert( {mapentry.first,graphlist} );
 	}
-	fragment_map.insert( {mapentry.first,graphlist} );
+	// For phsical operators just create a graph for each remaining edge
+	else if ( mapentry.first.get_operatortype() == OperatorType::physical ) {
+	    if ( mapentry.second.empty() )
+		continue;
+	    std::deque<std::pair<Graph, std::set<Vertex>>> graphlist;
+	    for ( auto& edge : mapentry.second ) {
+		Graph new_fragment{ { edge } };
+		auto subtracted{ subtract_connections( all_vertices, new_fragment.get_vertices() ) };
+		assert( subtracted.second && "Basic_cluster_fragments(..) : failed to subtract connections." );
+		graphlist.push_back( { { new_fragment } , subtracted.first } );
+	    }
+	    fragment_map.insert( {mapentry.first, graphlist} );
+	}
     }
     return fragment_map;
 }
 
 
-std::tuple<Graph, std::set<Vertex>,bool> join_graphs( 
-	const Graph &lhs_graph, const Graph &rhs_graph, const std::set<Vertex> &remaining_vertices )
-{
-	    // A list of vertices, whose edge_table resembles the connections that can still be formed.
-	    std::set<Vertex> new_remaining_vertices{ remaining_vertices };
-	    // From the above list, subtract the connections of lhs.
-	    for ( auto& lhs_vertex : lhs_graph.get_vertices() ) {
-		auto lhs_node = new_remaining_vertices.extract( lhs_vertex );
-		if ( !lhs_node.value().decrease_connection( lhs_vertex ) )
-		    return {lhs_graph,remaining_vertices,false};
-		new_remaining_vertices.insert( std::move( lhs_node ) );
-	    }
-	    // Moving on to the rhs. We need to check if we have open connections from the lhs left,
-	    // so we can join lhs and rhs.
-	    for ( auto& rhs_vertex : rhs_graph.get_vertices() ) {
-		auto rhs_node = new_remaining_vertices.extract( rhs_vertex );
-		if ( !rhs_node.value().decrease_connection( rhs_vertex ) )
-		    return {lhs_graph,remaining_vertices,false};
-		new_remaining_vertices.insert( std::move( rhs_node ) );	
-	    }
-	    // If we haven't returned by now, we're good to join the graphs.		
-	    Graph new_lhs_graph{ lhs_graph + rhs_graph };
-	    return { new_lhs_graph, new_remaining_vertices, true };
-}
     
-std::deque<Graph> compound_cluster_fragments( std::deque<Graph> basic_fragments, const std::set<Vertex> &all_vertices ) {
-    std::deque<Graph> outgraphs;
-    auto frag_it{ basic_fragments.cbegin() };
-    for ( auto& lhs_fragment : basic_fragments ) {
-	std::deque<Graph> lhs_graphs{ {lhs_fragment} };
-	for ( ; frag_it != basic_fragments.cend() ; ++frag_it ) {
+
+std::tuple<Graph, std::set<Vertex>,bool> merge_graphs( 
+	const std::pair<Graph, std::set<Vertex>> &lhs_graph, const std::pair<Graph, std::set<Vertex>> &rhs_graph )
+{
+    //std::cout << "Attempting to join: " ;
+    //(std::get<0>(lhs_graph)).print_edges(); std::cout <<  " & "; rhs_graph.first.print_edges();
+	    // A set of vertices, whose edge_table resembles the connections that can still be formed.
+	    auto subtracted{ subtract_connections( lhs_graph.second, rhs_graph.first.get_vertices() ) };
+	    if ( !subtracted.second ) 
+		return { lhs_graph.first, lhs_graph.second, subtracted.second };
+	    // If we haven't returned by now, we're good to join the graphs.		
+	    Graph new_lhs_graph{ lhs_graph.first + rhs_graph.first };
+	    return { new_lhs_graph, subtracted.first, subtracted.second };
+}
+
+std::deque<std::pair<Graph, std::set<Vertex>>> asymmetric_combinations(
+	const std::deque<std::pair<Graph, std::set<Vertex>>> &lhs_graphs, const std::deque<std::pair <Graph, std::set<Vertex>>> &rhs_graphs )
+{
+    //std::cout << "Performing asymmetric combination: " << '\n';
+    std::deque<std::pair<Graph, std::set<Vertex>>> combined_graphs;
+    for ( auto& lhs_graph : lhs_graphs ) {
+	for ( auto& rhs_graph : rhs_graphs ) {
+	    auto merged{ merge_graphs( lhs_graph, rhs_graph ) };
+	    //std::cout << '\t' << "Attempting to merge "; lhs_graph.first.print_edges() ; std:: cout << " & "; rhs_graph.first.print_edges() ; std::cout << " , Result: " << std::get<2>(merged) << '\n';
+	    if ( std::get<2>(merged) )
+		combined_graphs.push_back( { std::get<0>(merged) , std::get<1>(merged) } );
 	}
     }
-    return outgraphs;
+    return combined_graphs;
+}
+    
+
+std::deque<std::pair<Graph, std::set<Vertex>>> symmetric_combinations(
+	const std::deque<std::pair<Graph, std::set<Vertex>>> &lhs_graphs )
+{
+    //std::cout << "Performing symmetric combination: " << '\n';
+    std::deque<std::pair<Graph, std::set<Vertex>>> combined_graphs;
+    auto it_rhs{ lhs_graphs.cbegin() };
+    for ( auto& lhs_graph : lhs_graphs ) {
+	for ( auto it_rhs_inner = it_rhs; it_rhs_inner != lhs_graphs.cend() ; ++it_rhs_inner ) {
+	    auto merged{ merge_graphs( lhs_graph, *it_rhs_inner ) };
+	    //std::cout << '\t' << "Attempting to merge "; lhs_graph.first.print_edges() ; std:: cout << " & "; it_rhs_inner->first.print_edges() ; std::cout << " , Result: " << std::get<2>(merged) << '\n';
+	    if ( std::get<2>(merged) )
+		combined_graphs.push_back( { std::get<0>(merged), std::get<1>(merged) } );
+	}
+	++it_rhs;
+    }
+    return combined_graphs;
 }
 
-	    
+std::deque<std::pair<Graph, std::set<Vertex>>> complete_cluster_combinations(
+	const Vertex &cluster, const std::deque<std::pair<Graph, std::set<Vertex>>> &basic_fragments ) 
+{
+    // Splits the degree in even (combine fragment with itself) or odd (add a basic fragment to the combined) combinations.
+    std::stack<bool> divisible_by_two;
+    int degree{ cluster.degree() };
+    if ( (cluster.get_operatortype() == OperatorType::physical) && (basic_fragments.size() < 2) )
+	degree = 1;
+    if ( degree == 1 )
+	return basic_fragments;
+    while ( degree > 2 ) {	
+	if ( degree % 2 ) {	// if odd
+	    degree -= 1;
+	    divisible_by_two.push( false );
+	} else {			// if even 
+	    degree /= 2;
+	    divisible_by_two.push( true );
+	}
+    }
+    
+    // Populate first time, case for degree == 2.
+    std::deque<std::pair<Graph, std::set<Vertex>>> complete_fragments{ symmetric_combinations( basic_fragments ) } ;
+
+    while ( !divisible_by_two.empty() ) {
+	if ( divisible_by_two.top() ) 
+	    complete_fragments = symmetric_combinations( complete_fragments );
+	else
+	    complete_fragments = asymmetric_combinations( complete_fragments, basic_fragments );
+	divisible_by_two.pop();
+    }
+
+    return complete_fragments;
+}
 
 
-//Graph join_graphs(
-//
-//void create_graphs(std::deque<Graph> &final_graphs,std::deque<Graph> &current_graphs, std::list<Vertex*> vertices, std::list<Edge*> edges) {
-//
-//};
+std::deque<Graph> remove_duplicates( const std::deque<Graph> &all_graphs ) {
+    std::list<std::pair<Graph,AdjacencyMatrix>> compare_list;
+    for ( auto& graph : all_graphs )
+	compare_list.push_back( std::make_pair( graph, AdjacencyMatrix( graph ) ) );
+    // Graphs are identical if adjacency matrices are equal.
+    for ( auto it_list = compare_list.begin() ; it_list != compare_list.end() ; ++it_list ) {
+	auto it_seek = it_list;
+	++it_seek;
+	for ( ; it_seek != compare_list.end() ; ) {
+	    if ( it_list->second == it_seek->second )
+		it_seek = compare_list.erase( it_seek );
+	    else
+		++it_seek;
+	}
+ 
+    }
+    std::deque<Graph> outqueue;
+    for ( auto& element : compare_list )
+	outqueue.push_back( element.first );
+
+    return outqueue;
+}
+
+
+void search_connected( const Vertex vertex, std::set<Vertex> &to_visit, const AdjacencyMatrix &adj_mat ) {
+    // Since this function got called, we could reach the current vertex already,
+    // so remove it from the set of vertices that are still to be visited.
+    to_visit.erase( vertex );
+    // To which vertex can we go?
+    // Look for forward connected vertices
+    for ( auto& v_to : adj_mat.m_matrix.at( vertex ) ) {
+	if ( (v_to.second > 0) && to_visit.contains(v_to.first) )
+	    search_connected( v_to.first, to_visit, adj_mat );
+    }
+    // Look for backward connected vertices
+    for ( auto& v_from : adj_mat.m_matrix ) {
+	if ( (v_from.second.at( vertex ) > 0) && to_visit.contains( v_from.first ) )
+	    search_connected( v_from.first, to_visit, adj_mat );
+    }
+}
+
+
+std::deque<Graph> remove_disconnected( const std::deque<Graph> &all_graphs ) {
+    std::deque<Graph> disc_removed;
+    for ( auto& graph : all_graphs ) {
+	std::set<Vertex> to_visit{ graph.get_vertices() };
+	AdjacencyMatrix adj_mat{ graph };
+	search_connected( *(to_visit.begin()), to_visit, adj_mat );
+	if ( to_visit.empty() )
+	    disc_removed.push_back( graph );
+    }
+
+    return disc_removed;
+}
+
+
+std::deque<Graph> create_graphs( const std::set<Vertex> &vertices )
+{
+    // At first, we create all the possible contractions that can be created from the given vertices.
+    std::deque<Edge> all_edges{ create_edges( vertices ) };
+    
+    // To each cluster- or physical operator are edges, that begin or end at the respective operator, mapped.
+    std::map<Vertex, std::deque<Edge>> edge_map{ basic_cluster_edges( vertices, all_edges ) };
+
+    // For each cluster vertex, a queue with all possible, unique fragments, that can be created from the
+    // vertex, is created. Each fragment is paired with a set of vertices, that holds the connections which
+    // are not consumed yet by contractions.
+    std::map<Vertex,std::deque<std::pair<Graph, std::set<Vertex>>>>
+	basic_fragments{ basic_cluster_fragments( edge_map, vertices ) };
+
+    // These basic fragments have to be further combined, but first with themselves (e.g. T1^2,
+    // two halves of T2). By doing that, we can abuse symmetry to avoid duplicate combinations.
+    std::map<Vertex, std::deque<std::pair<Graph, std::set<Vertex>>>> complete_fragments;
+    for ( auto& vertex : basic_fragments ) {
+	std::deque<std::pair<Graph, std::set<Vertex>>> combination{
+	    complete_cluster_combinations( vertex.first, vertex.second ) };
+	if ( !combination.empty() )
+	    complete_fragments.insert( { vertex.first, complete_cluster_combinations( vertex.first, vertex.second ) } ); 
+    }
+
+    // All we have left to do, is to combine the complete cluster fragments and lastly to combine the results
+    // with the leftovers from the physical operator fragments. External operators can be ignored, since they
+    // either have to contract with cluster- or physical operators or they wont yield a valid graph.
+    auto it_complete_fragments{ complete_fragments.crbegin() };
+    std::deque<std::pair<Graph, std::set<Vertex>>> complete_pairs{ it_complete_fragments->second };
+    ++it_complete_fragments;
+    for ( ; it_complete_fragments != complete_fragments.crend() ; ++it_complete_fragments ) {
+	complete_pairs = asymmetric_combinations( complete_pairs, it_complete_fragments->second );
+    }
+
+    // Lastly, we have to discard incomplete graphs, i.e. those graphs, whose associated vertices (in the pair,
+    // not the member) still have open connections ( their edge_table entries != 0).
+    std::deque<Graph> complete_graphs;
+    for ( auto& complete_pair : complete_pairs ) {
+	bool loose_ends{false};
+	for ( auto& vertex : complete_pair.second ) {
+	   if ( vertex.connections() ) {
+	       loose_ends = true;
+	       break;
+	   }
+	}
+	if ( !loose_ends )
+	    complete_graphs.push_back( complete_pair.first );
+    }
+
+    std::deque<Graph> duplicates_removed{ remove_duplicates( complete_graphs ) };
+
+    std::deque<Graph> disconnected_removed{ remove_disconnected ( duplicates_removed ) };
+
+    return complete_graphs;
+}
+
+
+
+
+std::deque<Graph> pull_apart( const std::deque<Graph> &complete_graphs ) {
+    std::deque<Graph> pulled_apart;
+    for (auto& graph : complete_graphs ) {
+	Graph new_graph;
+	const std::deque<Edge>& current_edges = graph.get_edges();
+	   for ( auto it_edge = current_edges.cbegin() ; it_edge != current_edges.cend() ; ) {
+	       Edge first_edge{ *it_edge };
+	       Edge second_edge{ *(it_edge+1) };
+	       assert( first_edge.get_to() == second_edge.get_from() && "Cannot pull apart different cluster op." );
+	       std::string new_name{ first_edge.get_to().get_name().substr(0,2) };
+
+	   }
+    }
+    return pulled_apart;
+}
--- creategraph.h
+++ creategraph.h
@@ -5,24 +5,25 @@
 #include <deque>
 #include <map>
 #include <list>
+#include <stack>
 #include <tuple>
 #include <iostream>
+#include <cassert>
 
 /** \defgroup creategraph Graph Creation
- * @{
- */
+ * @{ */
 
-/// Creates a queue of all possible edges from a given queue of vertices.
+/// Creates a queue of all possible edges from a given set of vertices.
 /** 
  * From the input vertices, all possible combinations of outgoing connections in the q-creation 
  * and incoming connections in the q-annihilation space, and outgoing connections in the
  * q-annihilation and incoming connections in the q-creation space, are created.
  */
-std::deque<Edge> create_edges(const std::deque<Vertex> &vertices);
+std::deque<Edge> create_edges(const std::set<Vertex> &vertices);
 
 
 /// Creates a map with cluster operators as keys and queues of edges from or to that operator as values.
-std::map<Vertex,std::deque<Edge>> basic_cluster_edges( const std::deque<Vertex> &vertices, const std::deque<Edge> &edges );
+std::map<Vertex,std::deque<Edge>> basic_cluster_edges( const std::set<Vertex> &vertices, const std::deque<Edge> &edges );
 
 
 
@@ -34,15 +35,65 @@ std::map<Vertex,std::deque<Edge>> basic_cluster_edges( const std::deque<Vertex> 
  *
  * Output:
  * 	Queue	All fragments of the key vertex, that can be created with
- * 		value edges
+ * 		value edges. The member vertices of the graphs store the actual connections
+ * 		within the graph in their connectiontable, while the associated set in the return pair
+ *		contains all vertices that have to be included in the final graph and their connectiontables
+ *		resemble the connections that can still be formed when the associated subgraph/fragment is
+ *		subtracted.
+ *
  */
-std::map<Vertex,std::deque<Graph>> basic_cluster_fragments( const std::map<Vertex,std::deque<Edge>> &vertexedges );
+std::map<Vertex,std::deque<std::pair<Graph, std::set<Vertex>>>> basic_cluster_fragments( const std::map<Vertex,std::deque<Edge>> &vertexedges, const std::set<Vertex> &all_vertices );
 
 
-std::deque<Graph> compound_cluster_fragments( std::deque<Graph> basic_fragments, const std::set<Vertex> &all_vertices );
+/// Tries to merge two graphs.
+/**
+ * Input:
+ * 	Graphs that are to be merged, including the vertex-sets that store the connections that can still be formed,
+ * 	when the connections (entries in the connectiontable) are subtracted from all vertices contained in a final graph.
+ *
+ * Output:
+ * 	If successful, returns the merged graph and vertex-set (see above) and true.
+ * 	If failed, returns the lhs graph, vertex-set and false.
+ * 	
+ * Compares the associated vertex-set of lhs (connections that can still be formed) with the member set of rhs (connections
+ * that are present in rhs graph). If the connections of rhs are not present in the associated set of lhs, the graphs cannot
+ * be merged.
+ *
+ */
+std::tuple<Graph, std::set<Vertex>,bool> merge_graphs( const std::pair<Graph, std::set<Vertex>> &lhs_graph, const std::pair<Graph, std::set<Vertex>> &rhs_graph );
+ 
+
+/// Creates all combinations of graphs.
+/**
+ * Creates all possible combinations of lhs and rhs graphs. Each entry of lhs is tried to merge with rhs.
+ */
+std::deque<std::pair<Graph, std::set<Vertex>>> asymmetric_combinations(
+	const std::deque<std::pair<Graph, std::set<Vertex>>> &lhs_graphs, const std::deque<std::pair <Graph, std::set<Vertex>>> &rhs_graphs );
 
 
-std::deque<Graph> create_graphs(
-	std::deque<Vertex> vertices);
+/// Creates unique 2-combinations of graphs.
+/**
+ * All entries of lhs are merged with entries of lhs. Creates only unique combinations.
+ * For example, the combination of entry 1 and 2 results in combination 1-2, that is the same
+ * as combination 2-1.
+ *
+ */
+std::deque<std::pair<Graph, std::set<Vertex>>> symmetric_combinations(
+	const std::deque<std::pair<Graph, std::set<Vertex>>> &lhs_graphs );
 
+
+/// Combines basic fragments.
+std::deque<std::pair<Graph, std::set<Vertex>>> complete_cluster_combinations(
+	const Vertex &cluster, const std::deque<std::pair<Graph, std::set<Vertex>>> &basic_fragments );
+
+/// Creates complete graphs that includes all combinations of vertices.
+std::deque<Graph> create_graphs( const std::set<Vertex> &vertices );
+
+/// Removes duplicate identical graphs.
+std::deque<Graph> remove_duplicates( const std::deque<Graph> &all_graphs );
+
+/// Removes graphs that contain disconnected fragments.
+std::deque<Graph> remove_disconnected( const std::deque<Graph> &all_graphs );
+
+//std::deque<Graph> pull_apart( const std::deque<Graph> &complete_graphs );
 /** @} */
